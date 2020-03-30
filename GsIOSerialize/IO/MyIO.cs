8bd546e0cedad62d5bc23771cff800b3603d3abd
wangongshen@gmail.com <wangongshen@gmail.com>
2020-03-30 18:31:54 +0800

    添加项目文件。

---
+++ GsIOSerialize/IO/MyIO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GsIOSerialize.IO
+{
+    public class MyIO
+    {
+        private static string LogPath = ConfigurationManager.AppSettings["LogPath"];
+        private static string LogMovePath = ConfigurationManager.AppSettings["LogMovePath"];
+
+
+        public static void Show() {
+            //{
+            //    if (!Directory.Exists(LogPath))
+            //    { //检测文件夹是否存在 
+            //        Console.WriteLine("文件夹不存在");
+            //        return;
+            //    }
+            //    //不存在也不会报错
+            //    DirectoryInfo directory = new DirectoryInfo(LogPath);
+            //    Console.WriteLine(string.Format("文件夹路径：{0}；文件夹创建时间： {1}；文件夹最后修改时间： {2}", directory.FullName,directory.CreationTime,directory.LastWriteTime));
+                
+            //    if (!File.Exists(Path.Combine(LogPath,"info.txt"))) {
+            //        Console.WriteLine("文件不存在");
+            //        return;
+            //    }
+            //    FileInfo fileInfo = new FileInfo(Path.Combine(LogPath, "info.txt"));
+            //    Console.WriteLine(string.Format("文件路径：{0}；文件创建时间： {1}；文件最后修改时间： {2}", directory.FullName, directory.CreationTime, directory.LastWriteTime));
+            //}
+            //{
+            //    if (!Directory.Exists(LogPath)) {
+            //        //一次性创建全部子路径
+            //        DirectoryInfo directoryInfo = Directory.CreateDirectory(LogPath);
+            //    }
+            //    //移动文件夹
+            //    Directory.Move(LogMovePath, LogPath);
+            //    //删除空文件夹
+            //    Directory.Delete(LogPath);
+            //}
+            { 
+            
+            }
+        }
+    }
+}
---
+++ GsIOSerialize/Program.cs
@@ -0,0 +1,28 @@
+﻿using GsIOSerialize.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GsIOSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				Console.WriteLine("==========IO序列化==========");
+				{
+					MyIO.Show();
+				}
+			}
+			catch (Exception Ex)
+			{
+				Console.WriteLine("错误："+Ex.Message);
+			}
+			Console.ReadLine();
+        }
+    }
+}
